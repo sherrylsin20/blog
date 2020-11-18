fe60134a27a7d82a7e5316a68c20bbdecd91c299
Sherryl <sherrylsin20@gmail.com>
2020-11-18 08:09:12 +0700

    JWT implementation

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email|unique:users',
+            'username' => 'required|min:6',
+            'password' => 'required|min:6'
+        ]);
+
+        $attributes = [
+            'email' => $request->email,
+            'username' => $request->username,
+            'password' => app('hash')->make($request->password)
+        ];
+
+        $user = User::create($attributes);
+        return $user;
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        $credentials = request(['username', 'password']);
+        if(!$token = auth()->attempt($credentials)) {
+            return response()->json([
+                'msg' => 'login gagal'
+            ], 401);
+        } else {
+            return response()->json([
+                'access_token' => $token,
+                'token_type' => 'bearer',
+                'expires_in' => auth()->factory()->getTTL() * 60
+            ], 200);
+        }
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        return response()->json([
+            'msg' => 'Logout berhasil'
+        ], 200);
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group([
+    'prefix' => 'auth',
+], function() use ($router) {
+    $router->post('/register', 'UserController@register');
+    $router->post('/login', 'UserController@login');
+
+    $router->get('/logout', 'UserController@logout');
+});
